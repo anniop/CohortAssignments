b963266d2283c5a9e32de91302a4e1eee7da8b09
anniop <b.c114303@gmail.com>
2024-09-24 18:01:43 +0530

    COHORT Assignments

---
+++ week-2/week-2-js/easy/anagram.js
@@ -0,0 +1,27 @@
+/*
+  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
+  What's Anagram?
+  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
+*/
+
+// sort string 1
+// sort string 2
+// if str1 === str2 => true
+// else => false
+
+function isAnagram(str1, str2) {
+  const sortedString1 = str1.toLowerCase().split("").sort().join("");
+
+  const sortedString2 = str2.toLowerCase().split("").sort().join("");
+
+
+
+  if (sortedString1 == sortedString2){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+module.exports = isAnagram;
